0c824c4d94c0c829f4fc4a43cc7ee2d72c3883be
Anton Gadimba <antonhck@gmail.com>
2022-07-06 14:29:30 +0300

    Added user seeder #2

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@aurum.md',
+            'password' => bcrypt('win32points'),
+            'role' => User::ROLE_ADMIN,
+        ]);
+    }
+}
