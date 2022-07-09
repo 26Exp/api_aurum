01fbb4e15ff93dba623c7daa7c889cf3a71d758d
Anton Gadimba <antonhck@gmail.com>
2022-07-09 08:23:03 +0300

    Added languages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(LanguageSeeder::class);
         $this->call(AdminUserSeeder::class);
         $this->call(UserSeeder::class);
     }
