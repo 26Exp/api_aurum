99cfb45c0426745fff24a77bcba8cca9d557706c
Anton Gadimba <antonhck@gmail.com>
2022-08-22 17:43:28 +0300

    Adjust Compsoer json

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\MessageFormatter;
+use Maib\MaibApi\MaibClient;
+
 class OrderController extends Controller
 {
     /**
@@ -190,10 +198,74 @@ class OrderController extends Controller
 
     public function pay(Order $order)
     {
-        dd($order);
-       //  show server ip address and other info
-        $ip = $_SERVER['REMOTE_ADDR'];
-        return $order;
+        if ((isset($log_is_required) && $log_is_required)) {
+            $log = new Logger('maib_guzzle_request');
+            $log->pushHandler(new StreamHandler(__DIR__.'/maib_guzzle_request.log', Logger::DEBUG));
+            $stack = HandlerStack::create();
+            $stack->push(
+                Middleware::log($log, new MessageFormatter(MessageFormatter::DEBUG))
+            );
+        }
+
+        $options = [
+            'base_uri' => MaibClient::MAIB_TEST_BASE_URI,
+            'debug'  => false,
+            'verify' => true,
+            'cert'    => [MaibClient::MAIB_TEST_CERT_URL, MaibClient::MAIB_TEST_CERT_PASS],
+            'ssl_key' => MaibClient::MAIB_TEST_CERT_KEY_URL,
+            'config'  => [
+                'curl'  =>  [
+                    CURLOPT_SSL_VERIFYHOST => 2,
+                    CURLOPT_SSL_VERIFYPEER => true,
+                ]
+            ]
+        ];
+        if (isset($stack)) {
+            $options['handler'] = $stack;
+        }
+        $guzzleClient = new Client($options);
+        $client = new MaibClient($guzzleClient);
+
+        // The Parameters required to use MaibClient methods
+        $amount = 1; // The amount of the transaction
+        $currency = 978; // The currency of the transaction - is the 3 digits code of currency from ISO 4217
+        $clientIpAddr = '127.0.0.1'; // The client IP address
+        $description = 'testing'; // The description of the transaction
+        $lang = 'en'; // The language for the payment gateway
+
+        // Other parameters
+        $sms_transaction_id = null;
+        $dms_transaction_id = null;
+        $redirect_url = MaibClient::MAIB_TEST_REDIRECT_URL . '?trans_id=';
+        $sms_redirect_url = '';
+        $dms_redirect_url = '';
+
+        // The Parameters required to use MaibClient methods
+        $amount = 1; // The amount of the transaction
+        $currency = 978; // The currency of the transaction - is the 3 digits code of currency from ISO 4217
+        $clientIpAddr = '127.0.0.1'; // The client IP address
+        $description = 'testing'; // The description of the transaction
+        $lang = 'en'; // The language for the payment gateway
+
+        // Other parameters
+        $sms_transaction_id = null;
+        $dms_transaction_id = null;
+        $redirect_url = MaibClient::MAIB_TEST_REDIRECT_URL . '?trans_id=';
+        $sms_redirect_url = '';
+        $dms_redirect_url = '';
+
+
+        // The register dms authorization method
+        $registerDmsAuthorization = $client->registerDmsAuthorization($amount, $currency, $clientIpAddr, $description, $lang);
+        dd($registerDmsAuthorization);
+        $dms_transaction_id = $registerDmsAuthorization["TRANSACTION_ID"];
+        $dms_redirect_url = $redirect_url . $dms_transaction_id;
+
+        // The execute dms transaction method
+        $makeDMSTrans = $client->makeDMSTrans($dms_transaction_id, $amount, $currency, $clientIpAddr, $description, $lang);
+
+        dd($makeDMSTrans);
+
     }
 
     public function paymentCallback(Request $request)
