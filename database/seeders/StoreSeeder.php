8a2b6b1b019afcd3c79d6601a0dc397cccad675e
Anton Gadimba <antonhck@gmail.com>
2022-09-16 11:55:26 +0300

    Added stores in seeder

--- database/seeders/StoreSeeder.php
+++ database/seeders/StoreSeeder.php
@@ -16,9 +16,24 @@ class StoreSeeder extends Seeder
     public function run()
     {
         Store::create([
-            'name' => 'Aurum Balti',
+            'name' => 'Bălți — str. Independenței, 12',
             'latitude' => '47.76068884404603',
             'longitude' => '27.92691577104401',
+        ]);
+        Store::create([
+            'name' => 'Bălți — str. Ștefan cel Mare, 41',
+            'latitude' => '47.76155826986343',
+            'longitude' => '27.926256192526974',
+        ]);
+        Store::create([
+            'name' => 'Ungheni — str. Națională, 17',
+            'latitude' => '47.208305827735195',
+            'longitude' => '27.80160436896967',
+        ]);
+        Store::create([
+            'name' => 'Edineț — str. Mihai Eminescu, 1b',
+            'latitude' => '48.168742772797955',
+            'longitude' => '27.307183035310924',
         ]);
     }
 }
